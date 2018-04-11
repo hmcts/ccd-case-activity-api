0596826ff2b974a0761cc2f5a5f6c0598276ea6a
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-04-11 11:41:16 +0100

    fixed failing run script

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 
 var debug = require('debug')('ccd-case-activity-web:server');
 var http = require('http');
