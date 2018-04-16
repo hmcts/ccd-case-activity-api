e3db74913ecef1850b08035741451c21e70b2138
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-04-16 09:43:51 +0100

    renamed tests

--- app/user/white-black-list-roles-extractor.spec.js
+++ app/user/white-black-list-roles-extractor.spec.js
@@ -4,59 +4,41 @@ const extractor = require('./white-black-list-roles-extractor');
 chai.should();
 const expect = chai.expect;
 
-describe('white-black-list-roles-extractor', () => {
-  it('should invoke activity service and return response on successful requests', () => {
+describe('white-black-list-roles-extractor case single role', () => {
+  it('should not authorize when whitelist and blacklist are empty', () => {
     const authorised = extractor.getAuthorisedRoles(['caseworker-probate'], [], []);
     expect(authorised).to.deep.equal([]);
   });
 
-  it('should invoke activity service and return response on ', () => {
+  it('should authorize when is whitelisted and not blacklist is empty', () => {
     const authorised = extractor.getAuthorisedRoles(['caseworker-probate'], ['^caseworker-.+'], []);
     expect(authorised).to.deep.equal(['caseworker-probate']);
   });
 
-  it('should invoke activity service and return response on ', () => {
-    const authorised = extractor.getAuthorisedRoles(['probate'], ['^caseworker-.+'], []);
+  it('should not authorize when not whitelisted and blacklist is empty', () => {
+    const authorised = extractor.getAuthorisedRoles(['roleA'], ['^caseworker-.+'], []);
     expect(authorised).to.deep.equal([]);
   });
 
-  it('should invoke activity service and return response on ', () => {
+  it('should not authorize a blacklisted role ', () => {
     const authorised = extractor.getAuthorisedRoles(['caseworker-solicitor'], ['^caseworker-.+'], ['solicitor']);
     expect(authorised).to.deep.equal([]);
   });
 
-  it('should invoke activity service and return response on successful requests', () => {
+  it('should not authorize a role which is not whitelisted and not blacklisted', () => {
     const authorised = extractor.getAuthorisedRoles(['caseworker'], ['^caseworker-.+'], ['solicitor']);
     expect(authorised).to.deep.equal([]);
   });
+});
 
-  it('should invoke activity service and return response on successful requests', () => {
+describe('white-black-list-roles-extractor case multiple roles', () => {
+  it('should authorize when no blacklisted roles and some whitelisted', () => {
     const authorised = extractor.getAuthorisedRoles(['caseworker', 'caseworker-divorce'], ['^caseworker-.+'], ['solicitor']);
     expect(authorised).to.deep.equal(['caseworker', 'caseworker-divorce']);
   });
 
-  it('should invoke activity service and return response on successful requests', () => {
-    const authorised = extractor.getAuthorisedRoles(['caseworker', 'caseworker-divorce', 'caseworker-divorce-courtAdmin'], ['^caseworker-.+'], ['solicitor']);
-    expect(authorised).to.deep.equal(['caseworker', 'caseworker-divorce', 'caseworker-divorce-courtAdmin']);
-  });
-
-  it('should invoke activity service and return response on successful requests', () => {
+  it('should not authorize when some blacklisted', () => {
     const authorised = extractor.getAuthorisedRoles(['caseworker', 'caseworker-divorce', 'caseworker-divorce-solicitor'], ['^caseworker-.+'], ['solicitor']);
-    expect(authorised).to.deep.equal([]);
-  });
-
-  it('should invoke activity service and return response on successful requests', () => {
-    const authorised = extractor.getAuthorisedRoles(['caseworker', 'caseworker-divorce', 'caseworker-divorce-courtAdmin', 'otherRoles'], ['^caseworker-.+'], ['solicitor']);
-    expect(authorised).to.deep.equal(['caseworker', 'caseworker-divorce', 'caseworker-divorce-courtAdmin', 'otherRoles']);
-  });
-
-  it('should invoke activity service and return response on successful requests', () => {
-    const authorised = extractor.getAuthorisedRoles(['citizen'], ['^caseworker-.+'], ['solicitor']);
-    expect(authorised).to.deep.equal([]);
-  });
-
-  it('should invoke activity service and return response on successful requests', () => {
-    const authorised = extractor.getAuthorisedRoles(['otherRoles'], ['^caseworker-.+'], ['solicitor']);
     expect(authorised).to.deep.equal([]);
   });
 });
