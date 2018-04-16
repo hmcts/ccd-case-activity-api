a6b1a2ec7127afa8585e4b1fea624e9f34bb433a
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-04-16 15:40:58 +0100

    updated authorized to return boolean

--- app/user/authorised-roles-extractor.js
+++ app/user/authorised-roles-extractor.js
@@ -1,5 +1,6 @@
-require 
+const authorizer = require('./white-black-list-roles-authorizer');
+const config = require('config');
 
-const extract = (request, user) => undefined;
+const extract = (request, user) => authorizer.isUserAuthorized(user.roles, config.get('security.auth_whitelist'), config.get('security.auth_blacklist'));
 
 exports.extract = extract;
--- app/user/white-black-list-roles-authorizer.js
+++ app/user/white-black-list-roles-authorizer.js
@@ -1,13 +1,13 @@
-const debug = require('debug')('ccd-case-activity-web:white-black-list-roles-extractor');
+const debug = require('debug')('ccd-case-activity-web:white-black-list-roles-authorizer');
 
-const getAuthorisedRoles = (roles, whiteList, blackList) => {
+const isUserAuthorized = (roles, whiteList, blackList) => {
   const whitelisted = [].concat.apply(roles.filter(r => whiteList.some(w => r.match(w))));
   debug(`whitelisted: ${whitelisted}`);
 
   const blacklisted = [].concat.apply(roles.filter(r => blackList.some(w => r.match(w))));
   debug(`blacklisted: ${blacklisted}`);
 
-  return (whitelisted.length && !blacklisted.length) ? roles : [];
+  return whitelisted.length > 0 && blacklisted.length === 0;
 };
 
-exports.getAuthorisedRoles = getAuthorisedRoles;
+exports.isUserAuthorized = isUserAuthorized;
