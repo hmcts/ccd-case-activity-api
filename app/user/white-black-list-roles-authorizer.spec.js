09c649ceba92d10caf9fc5dc63916bcfbce95fa1
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-04-19 09:57:57 +0100

    rename

--- app/user/white-black-list-roles-authorizer.spec.js
+++ app/user/white-black-list-roles-authorizer.spec.js
@@ -6,39 +6,39 @@ const { expect } = chai;
 
 describe('white-black-list-roles-authorizer case single role', () => {
   it('should not authorize when whitelist and blacklist are empty', () => {
-    const authorised = authorizer.isUserAuthorized(['caseworker-probate'], [], []);
-    expect(authorised).to.be.false;
+    const authorized = authorizer.isUserAuthorized(['caseworker-probate'], [], []);
+    expect(authorized).to.be.false;
   });
 
   it('should authorize when is whitelisted and not blacklist is empty', () => {
-    const authorised = authorizer.isUserAuthorized(['caseworker-probate'], ['^caseworker-.+'], []);
-    expect(authorised).to.be.true;
+    const authorized = authorizer.isUserAuthorized(['caseworker-probate'], ['^caseworker-.+'], []);
+    expect(authorized).to.be.true;
   });
 
   it('should not authorize when not whitelisted and blacklist is empty', () => {
-    const authorised = authorizer.isUserAuthorized(['roleA'], ['^caseworker-.+'], []);
-    expect(authorised).to.be.false;
+    const authorized = authorizer.isUserAuthorized(['roleA'], ['^caseworker-.+'], []);
+    expect(authorized).to.be.false;
   });
 
   it('should not authorize a blacklisted role ', () => {
-    const authorised = authorizer.isUserAuthorized(['caseworker-solicitor'], ['^caseworker-.+'], ['solicitor']);
-    expect(authorised).to.be.false;
+    const authorized = authorizer.isUserAuthorized(['caseworker-solicitor'], ['^caseworker-.+'], ['solicitor']);
+    expect(authorized).to.be.false;
   });
 
   it('should not authorize a role which is not whitelisted and not blacklisted', () => {
-    const authorised = authorizer.isUserAuthorized(['caseworker'], ['^caseworker-.+'], ['solicitor']);
-    expect(authorised).to.be.false;
+    const authorized = authorizer.isUserAuthorized(['caseworker'], ['^caseworker-.+'], ['solicitor']);
+    expect(authorized).to.be.false;
   });
 });
 
 describe('white-black-list-roles-authorizer case multiple roles', () => {
   it('should authorize when no blacklisted roles and some whitelisted', () => {
-    const authorised = authorizer.isUserAuthorized(['caseworker', 'caseworker-divorce'], ['^caseworker-.+'], ['solicitor']);
-    expect(authorised).to.be.true;
+    const authorized = authorizer.isUserAuthorized(['caseworker', 'caseworker-divorce'], ['^caseworker-.+'], ['solicitor']);
+    expect(authorized).to.be.true;
   });
 
   it('should not authorize when some blacklisted', () => {
-    const authorised = authorizer.isUserAuthorized(['caseworker', 'caseworker-divorce', 'caseworker-divorce-solicitor'], ['^caseworker-.+'], ['solicitor']);
-    expect(authorised).to.be.false;
+    const authorized = authorizer.isUserAuthorized(['caseworker', 'caseworker-divorce', 'caseworker-divorce-solicitor'], ['^caseworker-.+'], ['solicitor']);
+    expect(authorized).to.be.false;
   });
 });
