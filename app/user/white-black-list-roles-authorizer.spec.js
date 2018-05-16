4f9b5ed7defad3043423c9476741f6c0019ed64a
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-05-16 08:53:21 +0100

    added eslint exclusing rules for know chai issue with eslint

--- app/user/white-black-list-roles-authorizer.spec.js
+++ app/user/white-black-list-roles-authorizer.spec.js
@@ -4,6 +4,8 @@ const authorizer = require('./white-black-list-roles-authorizer');
 chai.should();
 const { expect } = chai;
 
+/* eslint-disable no-unused-expressions */  
+
 describe('white-black-list-roles-authorizer case single role', () => {
   it('should not authorize when whitelist and blacklist are empty', () => {
     const authorized = authorizer.isUserAuthorized(['caseworker-probate'], [], []);
