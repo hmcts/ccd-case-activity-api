58a4a90a393088e2b9827937cee476b2fdd76eaa
Dwayne Bailey <dwayne@translate.org.za>
2018-07-17 11:27:23 -0700

    Set local cache to 700 MB
    /site - 300M
    /LogFiles - 400M
    
    Fixes RDM-2366

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,6 +20,7 @@ module "ccd-case-activity-api" {
   common_tags  = "${var.common_tags}"
   asp_name = "${(var.asp_name == "use_shared") ? local.sharedAppServicePlan : var.asp_name}"
   asp_rg = "${(var.asp_rg == "use_shared") ? local.sharedASPResourceGroup : var.asp_rg}"
+  website_local_cache_sizeinmb = 700
 
   app_settings = {
     CORS_ORIGIN_METHODS = "GET,POST,OPTIONS"
