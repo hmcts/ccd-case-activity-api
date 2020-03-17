0e84dd85db75e3539d9e8297dfafb79398c5408c
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-03-17 21:43:18 +0000

    Cleaning up terraform

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,11 +3,6 @@ variable "product" {
   default               = "ccd"
   description           = "The name of your application"
 }
-
-variable "raw_product" {
-  default = "ccd" // jenkins-library overrides product for PRs and adds e.g. pr-118-ccd
-}
-
 variable "location" {
   type                  = "string"
   default               = "UK South"
@@ -18,71 +13,6 @@ variable "env" {
   description           = "(Required) The environment in which to deploy the application infrastructure."
 }
 
-variable "ilbIp" {}
-
-variable "subscription" {}
-
-variable "capacity" {
-  default = "1"
-}
-
-variable "idam_api_url" {
-  default = "http://betaDevBccidamAppLB.reform.hmcts.net"
-}
-
-variable "cors_origin" {
-  default = "https://www-ccd.nonprod.platform.hmcts.net"
-}
-
-variable "auth_white_list" {
-  default = "^caseworker-.+"
-}
-
-variable "auth_black_list" {
-  default = "solicitor"
-}
-
-variable "redis_ssl_enabled" {
-  default = "true"
-}
-
-variable "redis_key_prefix" {
-  default = "activity:"
-}
-
-variable "redis_activity_ttl_sec" {
-  default = "5"
-}
-
-variable "redis_user_details_ttl_sec" {
-  default = "6000"
-}
-
-variable "app_request_timeout_sec" {
-  default = "5"
-}
-
-variable "app_store_cleanup_crontab" {
-  default = "* * * * *"
-}
-
 variable "common_tags" {
   type = "map"
-}
-
-variable "asp_name" {
-  type = "string"
-  description = "App Service Plan (ASP) to use for the webapp, 'use_shared' to make use of the shared ASP"
-  default = "use_shared"
-}
-
-variable "asp_rg" {
-  type = "string"
-  description = "App Service Plan (ASP) resource group for 'asp_name', 'use_shared' to make use of the shared resource group"
-  default = "use_shared"
-}
-
-variable "appinsights_instrumentation_key" {
-  description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
-  default = ""
 }
