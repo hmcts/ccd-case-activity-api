8f901867fc4c1d7f69ae83f01d2922639636171c
Paul Graham <paul.graham@hmcts.net>
2021-05-20 10:24:45 +0100

    EUI-2976
    Introduced a basic mechanism for accepting "socket" connections via socket.io and responding to various messages from the client. The code is messy and all inline at the moment but it needs to get into the repo sooner rather than later.

--- server.js
+++ server.js
@@ -25,6 +25,174 @@ app.set('port', port);
 var server = http.createServer(app);
 
 /**
+ * Set up a socket.io router.
+ */
+const io = require('socket.io')(server, {
+  allowEIO3: true
+});
+const IORouter = new require('socket.io-router-middleware');
+const iorouter = new IORouter();
+
+// const socketsWatchingCases = {};
+const caseStatuses = {};
+
+// Add router paths
+iorouter.on('/socket', (socket, ctx, next) => {
+  const payload = ctx.request.payload;
+  if (payload) {
+    if (payload.edit) {
+      handleEdit(socket, payload);
+    } else if (payload.view) {
+      handleView(socket, payload);
+    } else if (payload.watch) {
+      handleWatch(socket, payload.watch);
+    }
+  }
+  // ctx.response = { hello: 'from server' };
+  // socket.emit('response', ctx);
+  // Don't forget to call next() at the end to enable passing to other middlewares
+  next();
+});
+// On client connection attach the router
+io.on('connection', function (socket) {
+  socket.use((packet, next) => {
+    // Call router.attach() with the client socket as the first parameter
+    iorouter.attach(socket, packet, next);
+  });
+});
+
+function handleEdit(socket, payload) {
+  const caseId = payload.edit;
+  const stoppedViewing = stopViewingCases(socket.id);
+  if (stoppedViewing.length > 0) {
+    stoppedViewing.filter(c => c !== caseId).forEach(c => stopWatchingCase(socket, c));
+  }
+  const stoppedEditing = stopEditingCases(socket.id, caseId);
+  if (stoppedEditing.length > 0) {
+    stoppedEditing.filter(c => c !== caseId).forEach(c => stopWatchingCase(socket, c));
+  }
+  watchCase(socket, caseId);
+  const caseStatus = caseStatuses[caseId] || { viewers: [], editors: [] };
+  caseStatuses[caseId] = caseStatus;
+  const matchingEditor = caseStatus.editors.find(e => e.id === payload.user.id);
+  const notify = stoppedViewing.concat(stoppedEditing);
+  if (!matchingEditor) {
+    caseStatus.editors.push({ ...payload.user, socketId: socket.id });
+    notify.push(caseId);
+  }
+  if (notify.length > 0) {
+    notifyWatchers(socket, [ ...new Set(notify) ]);
+  }
+  socket.emit('response', getCaseStatuses([caseId]));
+}
+
+function handleView(socket, payload) {
+  const caseId = payload.view;
+  const stoppedViewing = stopViewingCases(socket.id, caseId);
+  if (stoppedViewing.length > 0) {
+    stoppedViewing.filter(c => c !== caseId).forEach(c => stopWatchingCase(socket, c));
+  }
+  const stoppedEditing = stopEditingCases(socket.id);
+  if (stoppedEditing.length > 0) {
+    stoppedEditing.filter(c => c !== caseId).forEach(c => stopWatchingCase(socket, c));
+  }
+  watchCase(socket, caseId);
+  const caseStatus = caseStatuses[caseId] || { viewers: [], editors: [] };
+  caseStatuses[caseId] = caseStatus;
+  const matchingViewer = caseStatus.viewers.find(v => v.id === payload.user.id);
+  const notify = stoppedViewing.concat(stoppedEditing);
+  if (!matchingViewer) {
+    caseStatus.viewers.push({ ...payload.user, socketId: socket.id });
+    notify.push(caseId);
+  }
+  if (notify.length > 0) {
+    notifyWatchers(socket, [ ...new Set(notify) ]);
+  }
+  socket.emit('response', getCaseStatuses([caseId]));
+}
+
+function handleWatch(socket, caseIds) {
+  watchCases(socket, caseIds);
+  socket.emit('cases', getCaseStatuses(caseIds));
+}
+
+function watchCases(socket, caseIds) {
+  caseIds.forEach(caseId => {
+    watchCase(socket, caseId);
+  });
+}
+function watchCase(socket, caseId) {
+  socket.join(`case:${caseId}`);
+}
+function stopWatchingCase(socket, caseId) {
+  socket.leave(`case:${caseId}`);
+}
+function notifyWatchers(socket, caseIds) {
+  caseIds.sort().forEach(caseId => {
+    socket.to(`case:${caseId}`).emit('cases', getCaseStatuses([caseId]));
+  });
+}
+
+function getCaseStatuses(caseIds) {
+  return caseIds.reduce((obj, caseId) => {
+    const cs = caseStatuses[caseId];
+    if (cs) {
+      obj[caseId] = {
+        viewers: [ ...cs.viewers.map(w => w.id) ],
+        editors: [ ...cs.editors.map(e => e.id) ]
+      };
+    }
+    return obj;
+  }, {});
+}
+
+function stopViewingOrEditing(socketId, exceptCaseId) {
+  const stoppedViewing = stopViewingCases(socketId, exceptCaseId);
+  const stoppedEditing = stopEditingCases(socketId, exceptCaseId);
+  return { stoppedViewing, stoppedEditing };
+}
+function stopViewingCases(socketId, exceptCaseId) {
+  const affectedCases = [];
+  Object.keys(caseStatuses).filter(key => key !== exceptCaseId).forEach(key => {
+    const c = caseStatuses[key];
+    const viewer = c.viewers.find(v => v.socketId === socketId);
+    if (viewer) {
+      c.viewers.splice(c.viewers.indexOf(viewer), 1);
+      affectedCases.push(key);
+    }
+  });
+  return affectedCases;
+}
+function stopEditingCases(socketId, exceptCaseId) {
+  const affectedCases = [];
+  Object.keys(caseStatuses).filter(key => key !== exceptCaseId).forEach(key => {
+    const c = caseStatuses[key];
+    const editor = c.editors.find(e => e.socketId === socketId);
+    if (editor) {
+      c.editors.splice(c.editors.indexOf(editor), 1);
+      affectedCases.push(key);
+    }
+  });
+  return affectedCases;
+}
+
+const connections = [];
+io.sockets.on("connection", (socket) => {
+  connections.push(socket);
+  console.log(" %s sockets is connected", connections.length);
+  // console.log('connections[0]', connections[0]);
+  socket.on("disconnect", () => {
+    console.log(socket.id, 'has disconnected');
+    stopViewingOrEditing(socket.id);
+    connections.splice(connections.indexOf(socket), 1);
+  });
+  socket.on("sending message", (message) => {
+    console.log("Message is received :", message);
+    io.sockets.emit("new message", { message: message });
+  });
+});
+
+/**
  * Listen on provided port, on all network interfaces.
  */
 
