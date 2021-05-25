942e4c20dc924a7d341719810418e5ac179e0b27
Paul Graham <paul.graham@hmcts.net>
2021-05-25 16:07:46 +0100

    Tests
    Moved redis keys and utils into separate files. The former is fully tested, the latter still needs tests for store and remove.

---
+++ test/spec/app/socket/redis-keys.spec.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect;
+const redisActivityKeys = require('../../../../app/socket/redis-keys');
+
+describe('socket.redis-keys', () => {
+
+  it('should get the correct key for viewing a case', () => {
+    const CASE_ID = '12345678';
+    expect(redisActivityKeys.view(CASE_ID)).to.equal(`case:${CASE_ID}:viewers`);
+  });
+
+  it('should get the correct key for editing a case', () => {
+    const CASE_ID = '12345678';
+    expect(redisActivityKeys.edit(CASE_ID)).to.equal(`case:${CASE_ID}:editors`);
+  });
+
+  it('should get the correct base key for a case', () => {
+    const CASE_ID = '12345678';
+    expect(redisActivityKeys.baseCase(CASE_ID)).to.equal(`case:${CASE_ID}`);
+  });
+
+  it('should get the correct key for a user', () => {
+    const USER_ID = 'abcdef123456';
+    expect(redisActivityKeys.user(USER_ID)).to.equal(`user:${USER_ID}`);
+  });
+
+  it('should get the correct key for a socket', () => {
+    const SOCKET_ID = 'zyxwvu987654';
+    expect(redisActivityKeys.socket(SOCKET_ID)).to.equal(`socket:${SOCKET_ID}`);
+  });
+
+});
