b30efab8e92549a41aec13976025792d0be51a0e
Paul Graham <paul.graham@hmcts.net>
2021-05-27 14:42:47 +0100

    Fixed the subscriptions
    The subscriptions were still looking for a 'case:' prefix but it's now 'c:'.

--- app/socket/redis/pub-sub.js
+++ app/socket/redis/pub-sub.js
@@ -1,16 +1,15 @@
-const ROOM_PREFIX = 'case:';
+const keys = require('./keys');
 
 module.exports = () => {
   return {
     init: (sub, caseNotifier)  => {
       if (sub && typeof caseNotifier === 'function') {
-        sub.psubscribe(`${ROOM_PREFIX}*`);
+        sub.psubscribe(`${keys.prefixes.case}:*`);
         sub.on('pmessage', (_, room) => {
-          const caseId = room.replace(ROOM_PREFIX, '');
+          const caseId = room.replace(`${keys.prefixes.case}:`, '');
           caseNotifier(caseId);
         });
       }
-    },
-    ROOM_PREFIX
+    }
   };
 };
