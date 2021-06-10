d24da5e7a5d10e955bc392fb3b2d64b2495b5b74
ridoutp <paul.ridout@cgi.com>
2021-06-10 09:25:32 +0100

    CCD-1245: Updated CCD definitions to 7.0.0
    - Changed build.gradle to reference ccd-test-definitions version 7.0.0
    instead of befta-fw version 6.14.2
    - Changed setup of TestDataLoaderToDefinitionStore in
    CaseActivityTestAutomationAdapter to use constructor that requires
    definitions path and store URL to be explicitly specified.
    
    These changes have been made following the splitting of CCD logic and
    data from the BEFTA framework core - see CCD-1223.

--- aat/src/java/uk/gov/hmcts/ccd/caseactivity/befta/CaseActivityTestAutomationAdapter.java
+++ aat/src/java/uk/gov/hmcts/ccd/caseactivity/befta/CaseActivityTestAutomationAdapter.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.ccd.caseactivity.befta;
 
+import uk.gov.hmcts.befta.BeftaMain;
 import uk.gov.hmcts.befta.BeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultBeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
@@ -7,7 +8,8 @@ import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
 
 public class CaseActivityTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
-    private TestDataLoaderToDefinitionStore loader = new TestDataLoaderToDefinitionStore(this);
+    private TestDataLoaderToDefinitionStore loader = new TestDataLoaderToDefinitionStore(this,
+            "uk/gov/hmcts/ccd/test_definitions/valid", BeftaMain.getConfig().getDefinitionStoreUrl());
 
     @Override
     protected BeftaTestDataLoader buildTestDataLoader() {
