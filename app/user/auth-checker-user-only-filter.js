179265687254f84b0289a1ad4d2063fba45b0851
mikebrownccd <104495891+mikebrownccd@users.noreply.github.com>
2022-11-21 10:45:20 +0000

    CCD-3838 Investigate & fix http 500 errors - created mapFetchErrors f… (#356)
    * CCD-3838 Investigate & fix http 500 errors - created mapFetchErrors function to return status 502 when error.message contains certain strings, tests added
    
    * lint issues fixed
    
    * next undefined check added, dev logging removed
    
    * next undefined checked corrected
    
    * unused res parameter removed from mapFetchErrors
    
    * for lint isBadGatewayError changed to an expression
    
    * next undefined check removed

--- app/user/auth-checker-user-only-filter.js
+++ app/user/auth-checker-user-only-filter.js
@@ -3,6 +3,29 @@ const userRequestAuthorizer = require('./user-request-authorizer');
 
 const logger = Logger.getLogger('authCheckerUserOnlyFilter');
 
+const isBadGatewayError = (error) => error.message !== undefined && (error.message.includes('getaddrinfo ENOTFOUND')
+  || error.message.includes('socket hang up')
+  || error.message.includes('getaddrinfo EAI_AGAIN')
+  || error.message.includes('connect ETIMEOUT')
+  || error.message.includes('ECONNRESET')
+  || error.message.includes('ECONNREFUSED'));
+
+const mapFetchErrors = (error, next) => {
+  if (isBadGatewayError(error)) {
+    next({
+      error: 'Bad Gateway',
+      status: 502,
+      message: error.message,
+    });
+  } else {
+    next({
+      error: 'Internal Server Error',
+      status: 500,
+      message: error.message,
+    });
+  }
+};
+
 const authCheckerUserOnlyFilter = (req, res, next) => {
   req.authentication = {};
 
@@ -15,11 +38,7 @@ const authCheckerUserOnlyFilter = (req, res, next) => {
     .catch((error) => {
       if (error.name === 'FetchError') {
         logger.error(error);
-        next({
-          status: 500,
-          error: 'Internal Server Error',
-          message: error.message,
-        });
+        mapFetchErrors(error, next);
       } else {
         logger.warn('Unsuccessful user authentication', error);
         error.status = error.status || 401; // eslint-disable-line no-param-reassign
