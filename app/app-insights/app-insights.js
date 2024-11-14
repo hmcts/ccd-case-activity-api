5bb96ee5a9baa6d11884391dbffc350197cba5b4
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2024-11-14 14:41:40 +0000

    Update app-insights.js

--- app/app-insights/app-insights.js
+++ app/app-insights/app-insights.js
@@ -8,9 +8,12 @@ const enableAppInsights = () => {
     return;
   }
   const appInsightsString = config.get('secrets.ccd.app-insights-connection-string');
+  const appInsightsRoleName = config.get('appInsights.roleName');
   appInsights.setup(appInsightsString)
     .setAutoDependencyCorrelation(true)
     .setAutoCollectConsole(true, true);
+  appInsights.defaultClient.context.tags[
+    appInsights.defaultClient.context.keys.cloudRole] = appInsightsRoleName;
   appInsights.defaultClient.config.samplingPercentage = 1;
   appInsights.start();
 };
