addb42d5fd69128f62ac2e763dae27ed97943301
Ankita Srivastava <ankita.srivastava@hmcts.net>
2026-09-17 11:03:19 +0100

    renovate/node-fetch-3.x

--- app/util/fetch.js
+++ app/util/fetch.js
@@ -1,6 +1,7 @@
-const nodeFetch = require('node-fetch');
+// node-fetch v3 is ESM-only, so load it dynamically from this CommonJS module.
+const nodeFetch = import('node-fetch').then(({ default: fetcher }) => fetcher);
 
-const fetch = (...args) => nodeFetch(...args)
+const fetch = (...args) => nodeFetch.then((fetcher) => fetcher(...args))
   .then((res) => {
     if (res.status >= 200 && res.status < 300) {
       return res;
