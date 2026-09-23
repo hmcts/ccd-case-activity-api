5041e6880bf17bf5ec355db0e81792760a1420dd
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2026-09-23 11:51:39 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,6 +19,12 @@ resource "azurerm_key_vault_secret" "redis_connection_string" {
   key_vault_id = data.azurerm_key_vault.shared.id
 }
 
+resource "azurerm_key_vault_secret" "managed_redis_password" {
+  name         = "activity-managed-redis-password"
+  value        = module.managed_redis_activity_service.primary_access_key
+  key_vault_id = data.azurerm_key_vault.shared.id
+}
+
 module "redis-activity-service" {
   source                        = "git@github.com:hmcts/cnp-module-redis?ref=4.x"
   product                       = "${var.product}-activity-service"
@@ -32,4 +38,28 @@ module "redis-activity-service" {
   sku_name                      = var.sku_name
   family                        = var.family
   capacity                      = var.capacity
+}
+
+module "managed_redis_activity_service" {
+  source = "git@github.com:hmcts/terraform-module-azure-managed-redis?ref=main"
+
+  product     = var.product
+  component   = var.component
+  env         = var.env
+  location    = var.location
+  common_tags = var.common_tags
+
+  sku_name = var.managed_redis_sku
+
+  public_network_access   = "Disabled"
+  create_private_endpoint = true
+  subnet_id               = data.azurerm_subnet.core_infra_redis_subnet.id
+  private_dns_zone_ids = [
+    "/subscriptions/${var.private_dns_subscription_id}/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.redis.azure.net"
+  ]
+
+  access_keys_authentication_enabled = true
+  client_protocol                    = "Encrypted"
+  clustering_policy                  = "NoCluster"
+  eviction_policy                    = "VolatileLRU"
 }
