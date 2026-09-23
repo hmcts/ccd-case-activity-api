56b9bf49074aace510df275cd12202f9eb2a4bf0
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2026-09-23 11:52:46 +0100

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -5,3 +5,11 @@ output "ccd-case-activity-api-redis-host" {
 output "ccd-case-activity-api-redis-port" {
   value = module.redis-activity-service.redis_port
 }
+
+output "ccd-case-activity-api-managed-redis-host" {
+  value = module.managed_redis_activity_service.hostname
+}
+
+output "ccd-case-activity-api-managed-redis-port" {
+  value = module.managed_redis_activity_service.port
+}
